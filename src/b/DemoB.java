4103c72061fb8ebe1c6e5e3de2d00cddab15ce76
Habteab <Habteab@habteab>
2024-01-04 18:14:52 -0500

    implement add method

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -8,4 +8,9 @@ public class DemoB {
 		 System.out.println("ProjectB we creating remote Repo");
 		 System.out.println("Sprint1 working");
 	}
+	
+	//git practice B automation
+	public static void add(int a , int b) {
+		System.out.println(a+b);
+	}
 }
