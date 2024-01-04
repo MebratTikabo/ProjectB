09eb7bcbbf4ea6c6a7c9802ebfe45cecc11998ec
Habteab <Habteab@habteab>
2024-01-03 21:19:36 -0500

    initial Commit

---
+++ src/b/DemoB.java
@@ -0,0 +1,11 @@
+package b;
+
+public class DemoB {
+
+	
+	public static void main(String[] args) {
+		 System.out.println("git practice B");
+		 System.out.println("ProjectB we creating remote Repo");
+		 System.out.println("Sprint1 working");
+	}
+}
